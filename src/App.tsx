89aaaf5eafbeab29478823a388c8c6f90464efdb
Vojtech Simetka <vojtech@simetka.cz>
2021-01-28 17:10:10 +0100

    fix: using both npm release package and official github

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import {Bee} from '@vojtechsimetka/bee-js'
+import {Bee} from '@ethersphere/bee-js'
+import {Bee as Bee2} from '@vojtechsimetka/bee-js'
 
 function App() {
   const [ file, setFile ] = useState<File|null>(null)
@@ -8,8 +9,12 @@ function App() {
     event.preventDefault();
     
     if (file) {
+      const f = new Uint8Array(await file.arrayBuffer())
       const bee = new Bee("http://localhost:1633");
-      console.log(await bee.uploadFile(new Uint8Array(await file.arrayBuffer())))
+      console.log("Github: ", await bee.uploadFile(f))
+
+      const bee2 = new Bee2("http://localhost:1633");
+      console.log("NPM: ", await bee2.uploadFile(f))
     }
   }
 
