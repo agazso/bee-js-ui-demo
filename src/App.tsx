a5aa9ce2035d74fc7ad8619661bf3c9fa4929bcb
Vojtech Simetka <vojtech@simetka.cz>
2021-01-28 16:54:09 +0100

    feat: simple single file upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {Bee} from '@vojtechsimetka/bee-js'
 
 function App() {
+  const [ file, setFile ] = useState<File|null>(null)
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>)=> {
+    event.preventDefault();
+    
+    if (file) {
+      const bee = new Bee("http://localhost:1633");
+      console.log(await bee.uploadFile(new Uint8Array(await file.arrayBuffer())))
+    }
+  }
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const f = e.target && e.target.files && e.target.files[0]
+
+    setFile(f)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="file" name="file" onChange={onFileChange} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
