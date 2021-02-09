4ef93d7800ed9cf4f2a5aea1e1922be88ddf72a0
Attila Gazso <agazso@gmail.com>
2021-02-09 15:11:28 +0100

    multiple files

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,29 @@ const beeUrl = "http://localhost:21633"
 const bee = new Bee(beeUrl);
 
 function App() {
-  const [ file, setFile ] = useState<File | null>(null)
+  const [ files, setFiles ] = useState<FileList | null>(null)
   const [ link, setLink ] = useState<string | null>(null)
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>)=> {
     event.preventDefault();
 
-    if (file) {
-      const hash = await bee.uploadFile(file)
-      const fileData = await bee.downloadFile(hash)
-      setLink(`${beeUrl}/files/${hash}`)
-      console.log({ file,  hash, fileData })
+    if (files) {
+      const hash = await bee.uploadFiles(files)
+      setLink(`${beeUrl}/dirs/${hash}`)
+      console.log({ files,  hash })
     }
   }
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const f = e.target && e.target.files && e.target.files[0]
+    const f = e.target && e.target.files && e.target.files
 
-    setFile(f)
+    setFiles(f)
   }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input type="file" name="file" onChange={onFileChange} />
+        <input type="file" name="file" multiple onChange={onFileChange} />
         <input type="submit" />
       </form>
       {
